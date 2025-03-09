463fed2934237b0bb3d83cefd22468f8dc2d554f
ac20qh <ac20qh@brocku.ca>
2025-03-09 10:21:48 -0400

    Dashboard Outline

--- src/inventory_track/inventory_track/settings.py
+++ src/inventory_track/inventory_track/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-#nq=v1&o%z_qak@p9cg%303%+em48%htbvp%0v2mxqn3l^lr+c
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,18 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'userauth',
     'messaging',
-<<<<<<< HEAD
     'updateStock',
     'manager',
     'notifications',
-=======
-<<<<<<< HEAD
     'inventoryApp',
-=======
-    'updateStock',
-    'manager',
->>>>>>> origin/main
->>>>>>> main
 ]
 
 MIDDLEWARE = [
@@ -84,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'inventory_track.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -92,13 +81,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'inventory_db',
-	'USER': 'user1',
-	'PASSWORD': 'password1',
-	'HOST': 'localhost',
-	'PORT': '3306',
+        'USER': 'user1',
+        'PASSWORD': 'password1',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -118,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -129,7 +116,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
