c41476e13092ddcfe6024a2e035bc2288eef07ce
ac20qh <ac20qh@brocku.ca>
2025-03-11 09:17:00 -0400

    More updates and fixes

--- src/inventory_track/updateStock/views.py
+++ src/inventory_track/updateStock/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from inventoryApp.models import InvItem  # Use the inventory management model
 from .forms import ProductStockUpdateForm
 
 @login_required
 def product_list(request):
-    """Display a list of all products with current stock levels."""
-    products = Product.objects.all()
+    """Display a list of all products from the inventory management table."""
+    products = InvItem.objects.all()
     return render(request, 'updateStock/product_list.html', {'products': products})
-
 
 @login_required
 def update_stock(request, pk):
     """Allow warehouse staff to update the stock of a specific product."""
-    product = get_object_or_404(Product, pk=pk)
+    # Use InvItem model here so we update the inventory management table
+    product = get_object_or_404(InvItem, pk=pk)
     if request.method == 'POST':
         form = ProductStockUpdateForm(request.POST, instance=product)
         if form.is_valid():
@@ -22,4 +22,3 @@ def update_stock(request, pk):
     else:
         form = ProductStockUpdateForm(instance=product)
     return render(request, 'updateStock/update_stock.html', {'form': form, 'product': product})
-
