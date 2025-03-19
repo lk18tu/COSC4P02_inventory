f29faa2ce40b24437773ce46ceb7ca18a732eb07
YukiNes <yh19ba@brocku.ca>
2025-03-18 20:37:21 -0400

    Fixed some errors caused by conflicts

--- src/inventory_track/inventoryApp/tests.py
+++ src/inventory_track/inventoryApp/tests.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 93dce4f (Added Inventory History feature with admin panel support)
--- src/inventory_track/inventoryApp/urls.py
+++ src/inventory_track/inventoryApp/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # inventoryApp/urls.py
 from django.urls import path
 from . import views
@@ -13,19 +12,3 @@ urlpatterns = [
     path("delete_item/<str:table_name>/<int:item_id>/", views.delete_item, name="delete_item"),
     path("add_custom_field/", views.add_custom_field, name="add_custom_field"),
 ]
-=======
-# inventoryApp/urls.py
-from django.urls import path
-from . import views
-
-app_name = "inventoryApp"
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("add_inventory/", views.add_inventory, name="add_inventory"),
-    path("add_item/<str:table_name>/", views.add_item, name="add_item"),
-    path("edit_item/<str:table_name>/<int:item_id>/", views.edit_item, name="edit_item"),
-    path("delete_item/<str:table_name>/<int:item_id>/", views.delete_item, name="delete_item"),
-    path("add_custom_field/", views.add_custom_field, name="add_custom_field"),
-]
->>>>>>> 93dce4f (Added Inventory History feature with admin panel support)
