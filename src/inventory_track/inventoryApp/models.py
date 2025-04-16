3fa16f658e526266fbd3e47828b3b9f031a457b1
ac20qh <ac20qh@brocku.ca>
2025-04-16 03:00:23 -0400

    UpdateStock(incomplete)
    UpdateStock tracking implementation so far.

--- src/inventory_track/inventoryApp/models.py
+++ src/inventory_track/inventoryApp/models.py
@@ -7,6 +7,17 @@ class InvItem(models.Model):
     quantity = models.IntegerField()
     custom_fields = models.JSONField(default=dict)
     image = models.ImageField(upload_to='inventory_images/', null=True, blank=True) # image field
+    tracking_id = models.CharField(max_length=20, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # First, call the parent save() to ensure an ID is assigned (if new)
+        is_new = self.pk is None
+        super().save(*args, **kwargs)
+        # If new and tracking_id is still not set, generate it (e.g., prefix with 't')
+        if is_new and not self.tracking_id:
+            self.tracking_id = f"t{1000 + self.id}"
+            # Update only the tracking_id field without triggering the full save again
+            self.__class__.objects.filter(pk=self.pk).update(tracking_id=self.tracking_id)
 
 def __str__(self):
     return self.title
